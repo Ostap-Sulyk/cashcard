8cfa5c32904074591825ac9d098faf787a23bec3
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-03-19 00:37:10 -0400

    Implemented post endpoint and passed all the tests

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,17 +1,17 @@
 package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
-    private CashCardRepository cashCardRepository;
+    private final CashCardRepository cashCardRepository;
 
     public CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
@@ -20,12 +20,21 @@ public class CashCardController {
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         Optional<CashCard> optionalCashCard = cashCardRepository.findById(requestedId);
-
-        if (optionalCashCard.isPresent()){
-            return ResponseEntity.ok(optionalCashCard.get());
-        }else{
-            return ResponseEntity.notFound().build();
-        }
-
+        return optionalCashCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PostMapping
+    public ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ubc){
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+
+        URI locationOfNewCashCard = ubc
+                .path("/cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+
+
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.net.URI;
 
 //this will allow our test to make requests to our application
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -40,4 +41,73 @@ class CashcardApplicationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
     }
+
+    @Test
+    void shouldCreateANewCashCard() {
+        // creating a cash card
+        CashCard newCashCard = new CashCard(null, 250.00);
+
+        // saving response from create operation         sending post request          sending card  expecting void to be a body
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+
+        // checking if status code of previously saved response is 201
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        // retrieving uri from previously saved response
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+
+        // now we have to check if received uri is correct by sending a get request using it
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        // if it was correct, our getResponse should have status code of OK because we managed to find created resource
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // already familiar document context and JsonPath
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(250.00);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
