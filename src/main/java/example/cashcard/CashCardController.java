4efc365da424690fdc07ffba0573d5ff710c956f
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-03-18 21:17:15 -0400

    refactored code

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,2 +1,31 @@
-package example.cashcard;public class RestController {
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+    private CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> optionalCashCard = cashCardRepository.findById(requestedId);
+
+        if (optionalCashCard.isPresent()){
+            return ResponseEntity.ok(optionalCashCard.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+    }
 }
--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,8 +1,6 @@
 package example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
